41c42a8a173c511b66267a38c85a5dbdbdc694bb
Afeez Bello <belloafeez7@gmail.com>
2025-07-10 18:29:59 +0100

    notis

--- src/routes/notificationRoutes.ts
+++ src/routes/notificationRoutes.ts
@@ -1,19 +1,24 @@
-import { Router } from 'express';
-import { authenticateToken } from '../middleware/auth';
+import { Router } from "express";
+import { authenticateToken } from "../middleware/auth";
 import {
   getNotifications,
   markNotificationAsRead,
   markAllNotificationsAsRead,
   deleteNotification,
-  getNotificationStats
-} from '../controllers/notificationController';
+  getNotificationStats,
+} from "../controllers/notificationController";
 
 const notificationRouter = Router();
 
-notificationRouter.get('/', authenticateToken, getNotifications);
-notificationRouter.put('/:id/read', authenticateToken, markNotificationAsRead);
-notificationRouter.put('/read-all', authenticateToken, markAllNotificationsAsRead);
-notificationRouter.delete('/:id', authenticateToken, deleteNotification);
-notificationRouter.get('/stats', authenticateToken, getNotificationStats);
+notificationRouter.get("/", authenticateToken, getNotifications);
+notificationRouter.get("/:id", authenticateToken, getNotification);
+notificationRouter.put("/:id/read", authenticateToken, markNotificationAsRead);
+notificationRouter.put(
+  "/read/all",
+  authenticateToken,
+  markAllNotificationsAsRead
+);
+notificationRouter.delete("/:id", authenticateToken, deleteNotification);
+notificationRouter.get("/stats", authenticateToken, getNotificationStats);
 
-export { notificationRouter }; 
+export { notificationRouter };
